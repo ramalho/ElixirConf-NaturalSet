5a36cdbd5b438cb5d648c3c8eaaddeeb80c8dddd
Luciano Ramalho <luciano@ramalho.org>
2020-08-30 17:21:21 -0300

    mix project and initial doctests

---
+++ natural_set_demo/lib/fibonacci.ex
@@ -0,0 +1,44 @@
+defmodule Fibonacci do
+  @doc ~S"""
+  Yields the Fibonacci sequence, limited only by available memory
+
+  ## Example
+
+   iex> Fibonacci.stream() |> Enum.take(10)
+   [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  """
+  def stream() do
+    Stream.unfold({0, 1}, fn {a, b} -> {a, {b, a + b}} end)
+  end
+
+  @doc ~S"""
+  Yields `count` numbers from the Fibonacci sequence.
+
+  ## Example
+
+   iex> Fibonacci.stream_count(10) |> Enum.to_list
+   [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  """
+
+  def stream_count(count) do
+    Stream.unfold({count, 0, 1}, fn
+      {0, _, _} -> nil
+      {count, a, b} -> {a, {count - 1, b, a + b}}
+    end)
+  end
+
+  @doc ~S"""
+  Yields numbers from the Fibonacci sequence up to `max`.
+
+  ## Example
+
+   iex> Fibonacci.stream_max(100) |> Enum.to_list
+   [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  """
+  def stream_max(max) do
+    Stream.unfold({0, 1}, fn
+      {a, _} when a > max -> nil
+      {a, b} -> {a, {b, a + b}}
+    end)
+  end
+end
---
+++ natural_set_demo/test/natural_set_demo_test.exs
@@ -0,0 +1,4 @@
+defmodule NaturalSetDemoTest do
+  use ExUnit.Case
+  doctest NaturalSetDemo
+end
