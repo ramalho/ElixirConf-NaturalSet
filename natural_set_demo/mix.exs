f8671735911d08d2be81aaf1aab61d3d53838316
Luciano Ramalho <luciano@ramalho.org>
2020-08-30 18:37:17 -0300

    generating docs

--- natural_set_demo/mix.exs
+++ natural_set_demo/mix.exs
@@ -4,23 +4,20 @@ defmodule NaturalSetDemo.MixProject do
   def project do
     [
       app: :natural_set_demo,
-      version: "0.1.0",
+      version: "2020.8.31",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
 
       # Docs
       name: "NaturalSetDemo",
       source_url: "https://github.com/ramalho/ElixirConf-NaturalSet/natural_set_demo",
       homepage_url: "https://hexdocs.pm/natural_set/api-reference.html",
       docs: [
-        main: "Main Page Title", # The main page in the docs
-        logo: "https://raw.githubusercontent.com/ramalho/ElixirConf-NaturalSet/master/img/N0.png",
+        logo: "img/N0.png",
         extras: ["README.md"]
       ]
   ]
-
-    ]
   end
 
   # Run "mix help compile.app" to learn about applications.
