d0d4e5e583ca0738e19fffea6708c7bb3d434759
Luciano Ramalho <luciano@ramalho.org>
2020-09-01 02:31:49 -0300

    minor docstring edit

--- protocol_demo/lib/protocol_demo.ex
+++ protocol_demo/lib/protocol_demo.ex
@@ -1,10 +1,9 @@
 defmodule ProtocolDemo do
   @moduledoc """
-  Demonstration: implementing `String.Chars` for `MapSet`:
+  Demonstration: implementing `String.Chars` for `MapSet`.
 
       iex> MapSet.new([4, 2, 1, 3]) |> to_string
       "{1 2 3 4}"
-
   """
 
   defimpl String.Chars, for: MapSet do
